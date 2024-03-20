5be390597581a0a1a0b496792e043a142bff7f94
hariharan0033 <hariharan00332810@gmail.com>
2024-03-20 09:28:22 +0530

    version 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+const number = document.getElementById("number")
+const incrementBtn = document.getElementById("btnin");
+const decrementBtn = document.getElementById("btndec");
+const resetBtn = document.getElementById("btnrs");
+let count = 0;
+
+
+incrementBtn.onclick = function(){
+    count++;
+    number.textContent = count;
+}
+
+decrementBtn.onclick = function(){
+    count--;
+    number.textContent = count;
+}
+resetBtn.onclick = function() {
+    count=0;
+    number.textContent = count;
+}
